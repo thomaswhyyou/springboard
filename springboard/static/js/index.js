6a41872ff14b9cfcaed5ffa37ba8d71aaa486211
Thomas <thomas.whyyou@gmail.com>
2015-12-05 05:20:19 +0000

    Add placeholder React component rendering for testing

--- springboard/static/js/index.js
+++ springboard/static/js/index.js
@@ -1,1 +1,17 @@
-alert("hello!");
+/*eslint indent: [2, 2]*/
+import React from "react";
+
+var Greeting = React.createClass({
+  render: function() {
+    return (
+      <div className="greeting">
+        Hello, {this.props.name}!
+      </div>
+    );
+  }
+});
+
+React.render(
+  <Greeting name="World"/>,
+  document.getElementById("react-mount-point")
+);
