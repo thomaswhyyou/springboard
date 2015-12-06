c5bee3b29353b928b788a9bcebff9ff7a47d29c4
Thomas <thomas.whyyou@gmail.com>
2015-12-06 19:57:30 +0000

    Add api/products endpoint with default response

--- springboard/views/api.py
+++ springboard/views/api.py
@@ -1,5 +1,7 @@
 from pyramid.view import view_config, view_defaults
 
+from springboard.models import sess, Product, Vendor
+
 
 @view_defaults(renderer="json")
 class APIViews:
@@ -7,6 +9,34 @@ class APIViews:
         self.request = request
 
     # api/products
-    @view_config(route_name='products')
-    def home(self):
-        return {"content": "Hello!"}
+    @view_config(route_name="products")
+    def products_list(self):
+        products = (
+            sess.query(Product)
+                .join(Vendor, Product.vendor_id == Vendor.id)
+                .order_by(Product.created_at.desc())
+                .limit(50)
+                .offset(0)
+        ).all()
+
+        resp = {
+            "offeset": 0,
+            "filters": {},
+            "data": [],
+        }
+        for product in products:
+            resp["data"].append({
+                "id": product.id,
+                "name": product.name,
+                "description": product.description,
+                "photo_url": product.photo_url,
+                "price": float(product.price),
+                "is_on_sale": product.is_on_sale,
+                "target_gender": product.target_gender,
+                "category": product.category,
+                "created_at": product.created_at.isoformat(),
+                "vendor_id": product.vendor_id,
+                "vendor_name": product.vendor.name,
+            })
+
+        return resp
